f641059b7c7a2e4afc3eedaf95e332a9d811d64d
OmarBara <omarbara00@yahoo.com>
2020-01-15 03:05:00 +0100

    30

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 allowNewServer= false;
+msg ='this is general massege from servers component'
+array = ['first', 'second', 'third']
   constructor() { 
     setTimeout(()=>{this.allowNewServer= true},2000)
   }
 
   ngOnInit() {
+  }
+  fun(){
+      this.msg = 'this XSERVER inside FUN';
+      this.array.push(this.msg)
+  }
+  updateTxt(event){
+    // console.log(event)
+    this.msg = event.target.value;
   }
 
 }
